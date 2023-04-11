b829da2b1b819de5223643f7f72342ae44428733
Garitskiy Victor <dinomc4711@gmail.com>
2023-04-11 15:37:26 +0600

    fix chip file

--- Thread.playground/Resources/Chip.swift
+++ Thread.playground/Resources/Chip.swift
@@ -1,26 +1,28 @@
 import Foundation
+import PlaygroundSupport
 
 //MARK: - Struct Chip
 
 public struct Chip {
+
     public enum ChipType: UInt32 {
         case small = 1
         case medium
         case big
     }
-    
-    //MARK: Make Chip
 
     public let chipType: ChipType
+    
+    //MARK: Make Chip
     
     public static func make() -> Chip {
         guard let chipType = Chip.ChipType(rawValue: UInt32(arc4random_uniform(3) + 1)) else {
             fatalError("Incorrect random value")
         }
-        
+
         return Chip(chipType: chipType)
     }
-
+    
     //MARK: Sodering Chip
 
     public func sodering() {
