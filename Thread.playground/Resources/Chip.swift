0eba41122261d88244eeeb8eccfe0bbcfde837f0
Garitskiy Victor <dinomc4711@gmail.com>
2023-04-11 15:00:42 +0600

    add Chip.swift

---
+++ Thread.playground/Resources/Chip.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+public struct Chip {
+    public enum ChipType: UInt32 {
+        case small = 1
+        case medium
+        case big
+    }
+    
+    public let chipType: ChipType
+    
+    public static func make() -> Chip {
+        guard let chipType = Chip.ChipType(rawValue: UInt32(arc4random_uniform(3) + 1)) else {
+            fatalError("Incorrect random value")
+        }
+        
+        return Chip(chipType: chipType)
+    }
+    
+    public func sodering() {
+        let soderingTime = chipType.rawValue
+        sleep(UInt32(soderingTime))
+    }
+}
+
+
