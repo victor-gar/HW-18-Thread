33d9fab65e58685c21386a69f81a9c37d2ecf9b3
Garitskiy Victor <dinomc4711@gmail.com>
2023-04-11 15:35:36 +0600

    add workThread class

--- Thread.playground/Resources/Work.swift
+++ Thread.playground/Resources/Work.swift
@@ -3,4 +3,30 @@ import Foundation
 //MARK: - Work Thread
 
 public class WorkThread: Thread {
+    
+    private let storage: Storage
+
+    public init(storage: Storage) {
+        self.storage = storage
+    }
+
+    public override func main() {
+        work()
+    }
+
+    private func work() {
+        for _ in 1...10 {
+            while (!GenerationThread.boolPredicate) {
+                GenerationThread.condition.wait()
+            }
+            storage.pop().sodering()
+            print("""
+            \n
+            Чип был припаян. Чип в хранилище \(storage.count)
+            """)
+            if storage.count < 1 {
+                GenerationThread.boolPredicate = false
+            }
+        }
+    }
 }
